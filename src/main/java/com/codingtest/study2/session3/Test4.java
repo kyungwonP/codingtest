4676be7fe3fc2f668ee11efb644d5dc2fac83bf1
parkkyungwon <kywonp@gmail.com>
2025-04-30 12:44:00 +0900

    feat: 섹션3 - Array(1, 2차원 배열) - 피보나치 수열
    피보나치 수열

---
+++ src/main/java/com/codingtest/study2/session3/Test4.java
@@ -0,0 +1,41 @@
+package com.codingtest.study2.session3;
+
+import java.util.Scanner;
+
+public class Test4 {
+    /**
+     * 2. 보이는 학생
+     * 설명
+     * 선생님이 N명의 학생을 일렬로 세웠습니다. 일렬로 서 있는 학생의 키가 앞에서부터 순서대로 주어질 때, 맨 앞에 서 있는
+     * 선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
+     * <p>
+     * 입력
+     * 첫 줄에 정수 N(5<=N<=100,000)이 입력된다. 그 다음줄에 N명의 학생의 키가 앞에서부터 순서대로 주어진다.
+     * <p>
+     * 출력
+     * 선생님이 볼 수 있는 최대학생수를 출력한다.
+     */
+    public static void main(String[] args) {
+        Test4 t = new Test4();
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        System.out.println(t.solution(number));
+    }
+
+    private String solution(int number) {
+        StringBuilder result = new StringBuilder();
+        int[] numbers = new int[number];
+
+        for (int i = 0; i < number; i++) {
+            if (i < 2) {
+                numbers[i] = 1;
+            } else {
+                numbers[i] = numbers[i - 1] + numbers[i - 2];
+            }
+
+            result.append(numbers[i]).append(" ");
+        }
+
+        return result.toString().trim();
+    }
+}
