1f416c61702b8facf8b6c184c4405cbb7b135d26
parkkyungwon <kywonp@gmail.com>
2025-05-29 19:51:47 +0900

    feat: 백준 - 1697 : 숨바꼭질 다국어
    bfs(너비 우선 탐색 (Breadth-First Search)), 시간복잡도 O(V + E) V:정점, E:간선(그래프가 빽빽해지면 E가 커지고 O(n)보다 커질 수 있음), 공간복잡도 O(n)

---
+++ src/main/java/com/codingtest/baekjoon/Test1697GPT.java
@@ -0,0 +1,71 @@
+package com.codingtest.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Test1697GPT {
+    /**
+     * 숨바꼭질 다국어 - bfs(너비 우선 탐색 (Breadth-First Search)), 시간복잡도 O(V + E) V:정점, E:간선(그래프가 빽빽해지면 E가 커지고 O(n)보다 커질 수 있음), 공간복잡도 O(n)
+     * 수빈이는 동생과 숨바꼭질을 하고 있다. 수빈이는 현재 점 N(0 ≤ N ≤ 100,000)에 있고, 동생은 점 K(0 ≤ K ≤ 100,000)에 있다.
+     * 수빈이는 걷거나 순간이동을 할 수 있다. 만약, 수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1로 이동하게 된다.
+     * 순간이동을 하는 경우에는 1초 후에 2*X의 위치로 이동하게 된다.
+     * 수빈이와 동생의 위치가 주어졌을 때, 수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인지 구하는 프로그램을 작성하시오.
+     * <p>
+     * 입력
+     * 첫 번째 줄에 수빈이가 있는 위치 N과 동생이 있는 위치 K가 주어진다. N과 K는 정수이다.
+     * <p>
+     * 출력
+     * 수빈이가 동생을 찾는 가장 빠른 시간을 출력한다.
+     */
+
+    private static final int MAX = 100000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(new Test1697GPT().solution(n, k));
+    }
+
+    private int solution(int n, int k) {
+        int second = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[MAX + 1];
+
+        queue.offer(n);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                int currentNumber = queue.poll();
+                if (currentNumber == k) {
+                    return second;
+                }
+
+                int[] moves = {currentNumber - 1, currentNumber + 1, currentNumber * 2};
+
+                for (int move : moves) {
+                    if (move < 0 || move > MAX || visited[move]) {
+                        continue;
+                    }
+
+                    visited[move] = true;
+                    queue.offer(move);
+                }
+            }
+
+            second++;
+        }
+
+        return second;
+    }
+}
